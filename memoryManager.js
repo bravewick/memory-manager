8337dfd8507e7191a6d20bbe681a9a77bcb72865
Konstantinos Tsolakis <konstantinos.tsolakis@accedo.tv>
2015-05-19 17:45:34 +0200

    next commit :D

--- memoryManager.js
+++ memoryManager.js
@@ -2,20 +2,30 @@
 // Not Allocated memory Ascii code: 45
 
 function MemoryManager(memory) {
-    this.freeBlocks = [{ index: 0, size: memory.length }, { index: 12, size: 20 }, { index: 50, size: 16 }];
+    this.freeBlocks = [{ index: 0, size: memory.length }];
     this.memory = memory;
 }
 
 MemoryManager.prototype.allocate = function (size) {
-
+    var allocatedBlock;
     var block = this.freeBlocks.filter(function (block) {
         return block.size >= size;
     }).sort(function(a, b) {
         return a.size - b.size;
     });
 
-    if (block !== -1) {
-        console.log('Free block found', block);
+    if (block.length) {
+        // console.log('Free block found', block);
+        if (block[0].length === size) {
+            allocatedBlock = this.freeBlocks.splice(this.freeBlocks.indexOf(block[0]), 1);
+        } else {
+            allocatedBlock = this.freeBlocks.splice(this.freeBlocks.indexOf(block[0]), 1, {
+                index: block[0].index + size,
+                size: block[0].size - size
+            });
+        }
+        // console.log('Allocated block:', allocatedBlock);
+        return allocatedBlock[0].index;
         // this.freeBlocks.splice()
         // return block;
     } else {
@@ -35,6 +45,14 @@ MemoryManager.prototype.read = function (size) {
 
 }
 
+MemoryManager.prototype.dump = function () {
+    var dump = '';
+    for(var i = 0; i < this.memory.length; i++) {
+        dump += typeof this.memory[i] === 'undefined' ? memoryManager.printASCIIchar(45) : memoryManager.printASCIIchar(927);
+    }
+    console.log(dump);
+}
+
 MemoryManager.prototype.printASCII = function (size) {
     var result = [];
     for (var i = 0; i < size; i++) {
@@ -44,12 +62,13 @@ MemoryManager.prototype.printASCII = function (size) {
 }
 
 MemoryManager.prototype.printASCIIchar = function (number) {
-    console.log(String.fromCharCode(number));
+    return String.fromCharCode(number);
 }
 
 var memoryArray = new Array(10);
 var memoryManager = new MemoryManager(memoryArray);
-memoryManager.allocate(10);
+console.log('Memory allocated at', memoryManager.allocate(10));
+memoryManager.dump();
 // MemoryManager.printASCIIchar(45);   // -
 // MemoryManager.printASCIIchar(927);  // O
 // MemoryManager.printASCIIchar(920);  // Θ
